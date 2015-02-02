4fbe8ffe8a0cdbaa5945813676c7074b4f3e085b
Vishal <vshldc@gmail.com>
2015-02-01 21:43:28 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,14 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Vishal Chandnani on 2/1/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+@property (nonatomic, assign) int totalAlive;
+@property (nonatomic, assign) int generation;
+@end
